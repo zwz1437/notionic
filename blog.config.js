6010877b2869dd4e164911ac87c945e45403277c
周文臻 <zhouwenzhen@xiaoyouzi.com>
2024-01-26 11:40:33 +0800

    字体

--- blog.config.js
+++ blog.config.js
@@ -31,7 +31,7 @@ const BLOG = {
   socialLink: {
     twitter: 'https://twitter.com/tanscp',
     github: 'https://github.com/zwz1437',
-    telegram: 'https://t.me/zuolan'
+    telegram: 'https://t.me/+WiMcsruJGgFmMDll'
   },
   seo: {
     keywords: ['Notionic', 'tanscp', 'Blog'],
@@ -44,7 +44,7 @@ const BLOG = {
   telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
   telegramChatId: '263895784', // The chat id of your Telegram bot
   telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
-  telegramChannelName: 'zuolan_me', // The name of your Telegram channel
+  telegramChannelName: 'tanscp', // The name of your Telegram channel
   craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
